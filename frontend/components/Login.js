35d35cc4da88bf8d40a62a034f74f016cf94200b
Dominic A.N. <dominicnunez@gmail.com>
2024-10-08 09:40:34 -0500

    switch to using custom hooks

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useInput } from '../hooks'
 
 const BASE_URL = 'http://localhost:9009/acme/auth'
 
 export default function Login() {
   const navigate = useNavigate()
   const [message, setMessage] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const onUsernameChange = e => setUsername(e.target.value)
-  const onPasswordChange = e => setPassword(e.target.value)
+  const username = useInput('username')
+  const password = useInput('password')
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+  // const onUsernameChange = e => setUsername(e.target.value)
+  // const onPasswordChange = e => setPassword(e.target.value)
   const onSubmit = async e => {
     e.preventDefault()
     const payload = {
-      username,
-      password,
+      username: username.value,
+      password: password.value,
     }
     try {
       const res = await axios.post(`${BASE_URL}/login`, payload)
@@ -34,14 +37,16 @@ export default function Login() {
         <input
           type="text"
           placeholder="username"
-          value={username}
-          onChange={onUsernameChange}
+          {...username}
+          // value={username}
+          // onChange={onUsernameChange}
         />
         <input
           type="password"
           placeholder="password"
-          value={password}
-          onChange={onPasswordChange}
+          {...password}
+          // value={password}
+          // onChange={onPasswordChange}
         />
         <button>Login</button>
       </form>
